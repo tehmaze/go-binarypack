b9ca2f872615dc0bee9268481706722c48cc65ba
Wijnand Modderman-Lenstra <maze@pyth0n.org>
2016-03-14 15:59:32 +0000

    Cleanup

---
+++ docs.go
@@ -0,0 +1,6 @@
+// Package binarypack implements the BinaryPack protocol, which is 95% msgpack.
+//
+// BinaryPack is derived from MsgPack, used in the Binary.js implementation at
+// https://github.com/binaryjs. It's modified to pack data more in line with
+// the JSON serialization format.
+package binarypack
